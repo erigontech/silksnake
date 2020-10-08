c0388c13ff27018ae13585280675945905b2b62f
canepat <tullio.canepa@gmail.com>
2020-10-08 02:31:46 +0200

    Add support for retrieving custom Turbo ETH supply Add unit tests for supply module

---
+++ silksnake/state/supply.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""The chain supply."""
+
+from ..core import kvstore
+from ..helpers.dbutils import tables
+from ..rlp import sedes
+
+def read_eth_supply(view: kvstore.View, block_number: int) -> int:
+    """ read_eth_supply """
+    encoded_block_number = sedes.encode_block_number(block_number)
+    key, value = view.get(tables.ETH_SUPPLY_LABEL, encoded_block_number)
+    if key != encoded_block_number:
+        raise RuntimeError('key mismatch, expected {0} got {1}'.format(encoded_block_number.hex(), key.hex()))
+    supply = int.from_bytes(value, 'big')
+    return supply
---
+++ tests/state/test_supply.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""The unit test for hashing module."""
+
+import pytest
+import pytest_mock
+
+from silksnake.state import supply
+
+@pytest.mark.parametrize("block_number,result_key,result_value,should_pass", [
+    # Valid test list
+    (0, '0000000000000000', '0000000000000001', True),
+    (1000000, '00000000000f4240', '', True),
+
+    # Invalid test list
+    (0, '', '', False),
+    (1000000, '', '', False),
+])
+def test_read_eth_supply(block_number: int, result_key: str, result_value: str, should_pass: bool):
+    """ Unit test for read_eth_supply. """
+    result_key_bytes = bytes.fromhex(result_key) if result_key is not None else None
+    result_value_bytes = bytes.fromhex(result_value) if result_value is not None else None
+    mock_view = pytest_mock.mock.Mock()
+    mock_view.get.return_value = result_key_bytes, result_value_bytes
+    if should_pass:
+        assert supply.read_eth_supply(mock_view, block_number) == int.from_bytes(result_value_bytes, 'big')
+    else:
+        with pytest.raises((RuntimeError)):
+            supply.read_eth_supply(mock_view, block_number)
